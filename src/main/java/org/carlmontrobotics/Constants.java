4996bdefbbba729f545333718ca5fb07da5c8f3c
AdhitDasDeepBlue <823573@seq.org>
2024-10-22 16:50:34 -0700

    Update Constants.java
    Added kP for PID (Proportional)

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -1,6 +1,10 @@
 package org.carlmontrobotics;
 
 public final class Constants {
+    //PID for the Drivetrain (DT)
+    //Not sure whether we need PID, but it would really help w/ precision
+    //Please tune the kP value so it doesn't oscillate too much bc momentu,
+    final dtKP = 0.75;
     // public static final class Drivetrain {
     //     public static final double MAX_SPEED_MPS = 2;
     // }
