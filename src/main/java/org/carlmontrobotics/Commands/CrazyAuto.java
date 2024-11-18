981d1c6336ca11c451beb3a14fdf8bf8d0d964d0
Niosocket11 <107586975+Niosocket11@users.noreply.github.com>
2024-11-17 18:39:19 -0800

    Made extra Autons(not done) that are simpler

---
+++ src/main/java/org/carlmontrobotics/Commands/CrazyAuto.java
@@ -0,0 +1,48 @@
+package org.carlmontrobotics.Commands;
+
+import java.util.ResourceBundle.Control;
+import java.util.function.DoubleSupplier;
+
+import org.carlmontrobotics.Subsystems.Drivetrain;
+import org.carlmontrobotics.Subsystems.Dumper;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Axis;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class CrazyAuto extends Command{
+    Timer timer = new Timer();
+    private final Drivetrain drivetrain;
+    double currentPos;
+    public CrazyAuto(Drivetrain drivetrain) {
+        addRequirements(this.drivetrain = drivetrain);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize()  {
+        timer.reset();
+        timer.start();
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return timer.get() > 15;
+    }
+}
